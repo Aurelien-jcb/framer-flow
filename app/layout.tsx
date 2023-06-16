fd294b4db0331e602628026a4cc61b10de510e72
ajacob <ajacob@atecna.fr>
2023-06-16 16:05:05 +0200

    remove metadata inside layout component

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,8 @@
 
 import "@/styles/globals.css"
 import { Fragment, useEffect, useState } from "react"
-import { Metadata } from "next"
 import { Transition } from "@headlessui/react"
 
-import { NavItem } from "@/types/nav"
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
 import { cn } from "@/lib/utils"
@@ -13,23 +11,6 @@ import SideBar from "@/components/layout/side-bar"
 import { TopBar } from "@/components/layout/top-bar"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
-
-export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
-  ],
-  icons: {
-    icon: "/favicon.ico",
-    shortcut: "/favicon-16x16.png",
-    apple: "/apple-touch-icon.png",
-  },
-}
 
 interface RootLayoutProps {
   children: React.ReactNode
